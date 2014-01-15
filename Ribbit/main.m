157411727332c799022999cb1571a02b306f38fa
Michael Fellows <michael.fellows12@gmail.com>
2014-01-15 16:02:30 -0500

    Initial Commit

---
+++ Ribbit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ribbit
+//
+//  Created by Michael Fellows on 1/15/14.
+//  Copyright (c) 2014 Michael Fellows. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
